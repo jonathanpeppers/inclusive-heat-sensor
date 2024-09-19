0baedbc3dd066e80982617ca91adba06b89ca32a
Wuyi Weng <wuweng@microsoft.com>
2024-09-19 11:28:17 -0700

    add client

--- Inclusive.HeatSensor.Web/Controllers/HeatSensorController.cs
+++ Inclusive.HeatSensor.Web/Controllers/HeatSensorController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Inclusive.HeatSensor.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Inclusive.HeatSensor.Web.Controllers
@@ -7,6 +7,15 @@ namespace Inclusive.HeatSensor.Web.Controllers
     [ApiController]
     public class HeatSensorController : ControllerBase
     {
+        readonly ILogger<HeatSensorController> _logger;
+        readonly LLMClient _client;
+
+        public HeatSensorController(ILogger<HeatSensorController> logger, LLMClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
         [HttpGet]
         public ActionResult<string> Get()
         {
@@ -14,11 +23,20 @@ namespace Inclusive.HeatSensor.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult<string> Post([FromBody] HeatSensorRequest requestBoday)
+        public async Task<ActionResult<string>> Post([FromBody] HeatSensorRequest requestBoday)
         {
             // Handle the comment here
+            _logger.LogInformation("heatsensor request");
 
-            return "Comment received: " + requestBoday.Comment;
+            if (string.IsNullOrEmpty(requestBoday.Comment))
+            {
+                _logger.LogInformation("comment is blank");
+                return new BadRequestObjectResult("Please provide a comment to rate.");
+            }
+
+            _logger.LogDebug("comment: " + requestBoday.Comment);
+            var rating = await _client.RateComment(requestBoday.Comment);
+            return new OkObjectResult(rating);
         }
     }
 }
