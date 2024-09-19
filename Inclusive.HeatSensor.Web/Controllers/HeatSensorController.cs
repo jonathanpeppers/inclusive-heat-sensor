5752bc628435f8dccce33f6ec280f9b443bf815e
Wuyi Weng <wuweng@microsoft.com>
2024-09-19 10:54:24 -0700

    add web project

---
+++ Inclusive.HeatSensor.Web/Controllers/HeatSensorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inclusive.HeatSensor.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeatSensorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "hello from Heat Sensor";
+        }
+    }
+}
