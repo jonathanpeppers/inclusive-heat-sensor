cc5d35c1f8c0937bb73ba6759237e3e0022de300
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-11-01 09:23:54 -0500

    [logging] add a url field, so we can avoid logging comments
    This way no comment text is logged if a URL is saved instead.

--- Inclusive.HeatSensor.Functions/HeatSensorFunction.cs
+++ Inclusive.HeatSensor.Functions/HeatSensorFunction.cs
@@ -18,15 +18,18 @@ public class HeatSensorFunction
     }
 
     [Function("heatsensor")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req, [FromBody] string comment)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req, [FromBody] string comment, [FromBody] string? url)
     {
-        _logger.LogInformation("heatsensor request");
+        _logger.LogDebug("heatsensor request");
         if (string.IsNullOrEmpty(comment))
         {
             _logger.LogInformation("comment is blank");
             return new BadRequestObjectResult("Please provide a comment to rate.");
         }
-        var rating = await _client.RateComment(comment);
+
+        // LogDebug shouldn't save the message
+        _logger.LogDebug("Request Comment: " + comment);
+        var rating = await _client.RateComment(comment, url);
         return new OkObjectResult(rating);
     }
 }
